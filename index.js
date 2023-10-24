1d8b2083784ee206b1aad7032f2a01f68a2f2f4e
hamsandwichh <145923931+hamsandwichh@users.noreply.github.com>
2023-10-24 13:02:19 +0300

    handling lec requests

--- index.js
+++ index.js
@@ -120,6 +120,84 @@ bot.action('matandop', ctx => {
 
 
 
+bot.action('progasem', ctx => {
+    let myfiles = listallfiles('sems/proga')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/sems/proga/" + f
+        })
+    }
+})
+
+bot.action('linalsem', ctx => {
+    let myfiles = listallfiles('sems/linal')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/sems/linal/" + f
+        })
+    }
+})
+bot.action('discrasem', ctx => {
+    let myfiles = listallfiles('sems/discra')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/sems/discra/" + f
+        })
+    }
+})
+
+bot.action('matansem', ctx => {
+    let myfiles = listallfiles('sems/matan')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/sems/matan/" + f
+        })
+    }
+})
+
+bot.action('linal', ctx => {
+    let myfiles = listallfiles('lects/linal')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/lects/linal/" + f
+        })
+    }
+})
+bot.action('discra', ctx => {
+    let myfiles = listallfiles('lects/discra')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/lects/discra/" + f
+        })
+    }
+})
+bot.action('matan', ctx => {
+    let myfiles = listallfiles('lects/matan')
+    console.log(myfiles)
+    for (let f of myfiles) {
+        bot.telegram.sendDocument(ctx.chat.id, {
+            source: "res/lects/matan/" + f
+        })
+    }
+})
+
+function listallfiles(a) {
+    console.log(__dirname + "/res/" + a)
+    let b = fs.readdirSync(__dirname + "/res/" + a, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        return files
+    });
+    return b
+}
+
 
 
 bot.launch()
